8fb7ff3aa15c19c7a6f2de03ea040d0f4a44ece1
Trịnh Hồng Anh <trinhhonganh21122005@gmail.com>
2024-09-26 15:18:25 +0700

    fix example 4/5

--- showTwoNumber.java
+++ showTwoNumber.java
@@ -7,11 +7,11 @@ public class showTwoNumber{
         String strNum1, strNum2;
         String Notification = "You've just entered: ";
         strNum1 = JOptionPane.showInputDialog(null, 
-                "PLease input the first message", 
+                "PLease input the first number", 
                 "Input the first number", 
                 JOptionPane.INFORMATION_MESSAGE);
         strNum2 = JOptionPane.showInputDialog(null, 
-                "PLease input the second message", 
+                "PLease input the second number", 
                 "Input the second number", 
                 JOptionPane.INFORMATION_MESSAGE);
         Notification += strNum1 + " and " + strNum2;
