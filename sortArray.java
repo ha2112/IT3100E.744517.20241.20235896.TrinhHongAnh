0f05316d956c9a5bd35c4261feacf8623c205d61
Trịnh Hồng Anh <trinhhonganh21122005@gmail.com>
2024-10-03 10:43:56 +0700

    Add sortArray exercise

---
+++ sortArray.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class sortArray {
+    public static void main(String args[]){
+        System.out.println("How many elements are there in your array?");
+        Scanner keyboard = new Scanner(System.in);
+        double total = 0;
+        int n = keyboard.nextInt();
+        System.out.println("Enter your array: ");
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = keyboard.nextInt();
+            total += arr[i];
+        }
+        double avg = total / n;
+        Arrays.sort(arr);
+        System.out.println("The sorted array is " + Arrays.toString(arr));
+        System.out.println("The average value of this array is " + String.valueOf(avg));
+        System.exit(0);
+    }
+}
