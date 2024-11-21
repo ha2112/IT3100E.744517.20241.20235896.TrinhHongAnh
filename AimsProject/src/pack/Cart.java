31a12a038d1f0562b127b56eaf1ccb66f751c42d
ha2112 <trinhhonganh21122005@gmail.com>
2024-11-21 16:55:02 +0700

    Change AimsProject Directory

---
+++ AimsProject/src/pack/Cart.java
@@ -0,0 +1,35 @@
+package pack;
+
+import java.util.ArrayList;
+
+public class Cart {
+    private ArrayList<DigitalVideoDisc> itemsOrdered = new ArrayList<>();
+    public final static int MAX_NUMBERS_ORDERED = 20;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (itemsOrdered.size() < MAX_NUMBERS_ORDERED) {
+            itemsOrdered.add(disc);
+            System.out.println("The disc has been added");
+            System.out.println("Current cart: " + itemsOrdered.size() + " discs");
+        } else {
+            System.out.println("The cart is full");
+        }
+    }
+
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc) {
+        if (itemsOrdered.size() == 0) {
+            System.out.println("The cart is empty");
+        } else {
+            itemsOrdered.remove(disc);
+            System.out.println("The disc has been removed");
+        }
+    }
+
+    public float totalCost() {
+        float total = 0;
+        for (DigitalVideoDisc disc : itemsOrdered) {
+            total += disc.getCost();
+        }
+        return total;
+    }
+}
